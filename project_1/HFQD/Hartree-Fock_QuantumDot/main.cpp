475e40865ec4deca7606c1f40efa35d9a56e4a96
andrei-fys <andreiku@fys.uio.no>
2017-03-11 13:42:59 +0100

    parameters from keyboard

--- project_1/HFQD/Hartree-Fock_QuantumDot/main.cpp
+++ project_1/HFQD/Hartree-Fock_QuantumDot/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include "quantumdot.h"
-#include "Coulomb_Functions.hpp"
+//#include "Coulomb_Functions.hpp"
 
 using namespace std;
 
-int main()
+int main(int numberOfArguments, char **argumentList)
 {
 
-    int NumberOfShells = 5;
-    int HOStrenth = 1;
+    int NumberOfShells = 3;
     int NumberOfElectrons = 6;
+    int HOStrenth = 1;
+
+
+    // If a first argument is provided, it is the number of shells
+    if(numberOfArguments > 1) NumberOfShells = atoi(argumentList[1]);
+    // If a second argument is provided, it is the number of electrons
+    if(numberOfArguments > 2) NumberOfElectrons = atoi(argumentList[2]);
+    // If a third argument is provided, it is the HO strenth /omega
+    if(numberOfArguments > 3) HOStrenth = atoi(argumentList[3]);
 
     QuantumDot qdot(NumberOfShells, HOStrenth, NumberOfElectrons);
     //qdot.getQuantumDotStates();
